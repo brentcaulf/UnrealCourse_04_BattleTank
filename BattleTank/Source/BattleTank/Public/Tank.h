793e77e145a8385ff225f332e5b2a4ee0c5d49e2
Brent Caulfield <brentcaulfield@gmail.com>
2018-10-30 14:47:26 -0400

    Lecture 151: Finishing up the turret rotation setup

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -39,6 +39,6 @@ private:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(EditAnywhere, Category = Firing)
-	float LaunchSpeed = 100000.0; // TODO find sensible default
+	float LaunchSpeed = 4000.0;
 
 };
