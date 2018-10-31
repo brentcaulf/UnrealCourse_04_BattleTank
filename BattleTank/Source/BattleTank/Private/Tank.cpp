0099f3c410418b5ffa2103222242b8f8770035f0
Brent Caulfield <brentcaulfield@gmail.com>
2018-10-31 10:49:21 -0400

    Lecture 157: Automatically adding a projectile movement component to our projectile and then launching it through the Tank's fire method and a new method within the projectile called LaunchProjectile which uses the Tank's LaunchSpeed as a velocity multiplier.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -20,16 +20,16 @@ void ATank::SetTurretReference(UTankTurret * TurretToSet)
 
 void ATank::Fire()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Fire!"));
-
 	if (!Barrel) { return; }\
 
 		// Spawn a projectile at the socket location on the barrel
-		GetWorld()->SpawnActor<AProjectile>(
+		auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 			ProjectileBlueprint,
 			Barrel->GetSocketLocation(FName("Projectile")),
 			Barrel->GetSocketRotation(FName("Projectile"))
 			);
+		
+		Projectile->LaunchProjectile(LaunchSpeed);
 }
 
 // Sets default values
