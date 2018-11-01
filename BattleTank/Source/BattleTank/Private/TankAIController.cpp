2a9e15e5c54c889ab3971e59f686cd5dcd07edf8
Brent Caulfield <brentcaulfield@gmail.com>
2018-11-01 11:13:58 -0400

    Lectures 168 and 169: Setting up our AI movement using MoveToActor from the AI Controller and then intercepting and overriding RequestDirectMove from the TankMovementComponent which inherits from the Nav Movement Component.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -27,7 +27,8 @@ void ATankAIController::Tick(float DeltaTime)
 	// Aim at the player tank
 	if (PlayerTank)
 	{
-		// TODO Move towards the player
+		// Move towards the player
+		MoveToActor(PlayerTank, AcceptanceRadius);
 
 		// Aim towards the player
 		ControlledTank->AimAt(PlayerTank->GetActorLocation());
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -20,4 +20,6 @@ private:
 
 	virtual void Tick(float DeltaTime) override;
 
+	// How close can the AI tank get to the player
+	float AcceptanceRadius = 3000;
 };
