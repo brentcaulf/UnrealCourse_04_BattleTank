986614ce64876e543083ce6b332c89b523b8b13d
Brent Caulfield <brentcaulfield@gmail.com>
2018-11-01 08:57:17 -0400

    Lecture 166: Creating a Intend to Move Forward method which calls on SetThrottle from both tracks and changing the way we communicate with the tracks by setting up a blueprint initialise method.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,13 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+
+}
 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
+{	
+	//UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO prevent double-speed due to dual control use
+}
+
+void UTankMovementComponent::IntendMoveRight(float Throw)
 {
 	
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
-
+	if (Throw > 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Intend move left: %f"), Throw);
+		LeftTrack->SetThrottle(Throw);
+	}
+	if (Throw < 0)
+	{ 
+		UE_LOG(LogTemp, Warning, TEXT("Intend move right: %f"), Throw);
+		RightTrack->SetThrottle(Throw);
+	}
 }
 
 
